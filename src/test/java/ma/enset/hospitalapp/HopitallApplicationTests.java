a36889d20fd6a9d6f07eb30b2d342fccc92d38ef
Mohamed-Idriss Naour <artgetaizar6@gmail.com>
2025-06-01 12:08:27 +0100

    first

---
+++ src/test/java/ma/enset/hospitalapp/HopitallApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.hospitalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopitallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
